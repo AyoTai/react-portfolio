24d565317e7803b7fbf81eec2c4d6061f72f17d2
Tai Mujarin <tai.mujarin@gmail.com>
2021-07-29 22:26:48 -0700

    added header component

---
+++ src/component/nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <header class="main-header">
+                <nav class="nav main-nav">
+                    <ul>
+                        <li><a href="#aboutme">About Me</a></li>
+                        <li><a href="#work">Work</a></li>
+                        <li><a href="#contactme">Contact Me</a></li>
+                        <li><a href="./resume.pdf" target="_blank">Resume</a></li>
+                    </ul>
+                </nav>
+                <h1 class="myname">Tai Mujarin</h1>
+                <hr></hr>
+        </header>
+    )
+}
+
+export default Nav;
