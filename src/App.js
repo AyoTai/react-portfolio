c61e5abffe19532c23fdc1f9c0495842f75ff4d7
Tai Mujarin <tai.mujarin@gmail.com>
2021-07-29 22:56:12 -0700

    added footer and hero img component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Nav from './component/nav';
-import './App.css';
+import Nav from './component/Nav';
+import Footer from './component/Footer';
+import Hero from './component/Hero';
+import './assets/css/styles.css'
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Hero />
 
+
+      <Footer />
     </div>
   );
 }
---
+++ src/component/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return(
+    <footer>
+        <h5 className="footer">Handcrafted with 💖 &#169; 2021</h5>
+    </footer>
+    )
+}
+
+export default Footer;
