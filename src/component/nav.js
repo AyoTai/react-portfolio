519f3ad4c98ac80dee6a3972ca3f68bf8cb3a221
Tai Mujarin <tai.mujarin@gmail.com>
2022-01-18 15:34:55 -0800

    fixing resume link

--- src/component/nav.js
+++ src/component/nav.js
@@ -8,10 +8,7 @@ function Nav() {
                     <li><a href="#aboutme">About Me</a></li>
                     <li><a href="#work">Work</a></li>
                     <li><a href="#contactme">Contact Me</a></li>
-<<<<<<< HEAD
                     <li><a href="./FullStack-Resume.pdf" target="_blank">Resume</a></li>
-=======
->>>>>>> 6070c600594c35ddb4eeb4ee270a6d0b3f91c202
                 </ul>
             </nav>
             <h1 className="myname">Tai Mujarin</h1>
